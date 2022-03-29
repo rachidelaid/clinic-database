a517f23b463cd774cff855a59e0bb0a42827b276
rachid elaid <rachidelaid@gmail.com>
2022-03-29 15:03:33 +0000

    create medical_histories table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,5 +1,16 @@
+-- create patients table
 CREATE TABLE patients (
   id SERIAL PRIMARY KEY,
   name VARCHAR(50),
   date_of_birth DATE
+);
+
+-- create medical_histories
+CREATE TABLE medical_histories (
+  id SERIAL PRIMARY KEY,
+  admitted_at TIME,
+  status VARCHAR(255),
+  FOREIGN KEY (patient_id) 
+    REFERENCES patients (id)
+    ON DELETE CASCADE
 )
