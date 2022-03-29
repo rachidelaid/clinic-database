a194d8557ae7a157ffaf80625491052325f5fb86
rachid elaid <rachidelaid@gmail.com>
2022-03-29 15:16:57 +0000

    create invoices table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -33,4 +33,32 @@ CREATE TABLE medical_histories_treatments (
   FOREIGN KEY (treatment_id) 
     REFERENCES treatments (id)
     ON DELETE CASCADE
-)
+);
+
+-- create invoices table 
+CREATE TABLE invoices (
+  id SERIAL PRIMARY KEY,
+  total_amount DECIMAL NOT NULL,
+  generated_at TIME NOT NULL,
+  payed_at TIME NOT NULL,
+  medical_history_id INT,
+  FOREIGN KEY (medical_history_id) 
+    REFERENCES medical_histories (id)
+    ON DELETE CASCADE
+);
+
+-- create invoice_items table
+CREATE TABLE invoice_items (
+  id SERIAL PRIMARY KEY,
+  unit_price DECIMAL NOT NULL,
+  quantity INT NOT NULL, 
+  total_price DECIMAL NOT NULL,
+  invoice_id INT,
+  treatment_id INT,
+  FOREIGN KEY (invoice_id) 
+    REFERENCES invoices (id)
+    ON DELETE CASCADE,
+  FOREIGN KEY (treatment_id) 
+    REFERENCES treatments (id)
+    ON DELETE CASCADE
+);
