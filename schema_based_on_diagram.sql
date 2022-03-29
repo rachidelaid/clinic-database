0833e2b9ab1bd1b2b5a1dc1d9e7c32ab573ad67e
rachid elaid <rachidelaid@gmail.com>
2022-03-29 15:18:48 +0000

    add NOT NULL constraint

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,16 +1,16 @@
 -- create patients table
 CREATE TABLE patients (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(50),
-  date_of_birth DATE
+  name VARCHAR(50) NOT NULL,
+  date_of_birth DATE NOT NULL
 );
 
 -- create medical_histories table
 CREATE TABLE medical_histories (
   id SERIAL PRIMARY KEY,
-  admitted_at TIME,
-  patient_id INT,
-  status VARCHAR(255),
+  admitted_at TIME NOT NULL,
+  patient_id INT NOT NULL,
+  status VARCHAR(255) NOT NULL,
   FOREIGN KEY (patient_id) 
     REFERENCES patients (id)
     ON DELETE CASCADE
@@ -19,8 +19,8 @@ CREATE TABLE medical_histories (
 -- craete treatments table
 CREATE TABLE treatments (
   id SERIAL PRIMARY KEY,
-  type VARCHAR(255),
-  name VARCHAR(50)
+  type VARCHAR(255) NOT NULL,
+  name VARCHAR(50) NOT NULL
 );
 
 -- create join table between treatments and medical_histories
