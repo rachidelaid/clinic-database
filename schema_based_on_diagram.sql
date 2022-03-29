592088a00bd94ad9fe932ad9abb49bc454fcac59
rachid elaid <rachidelaid@gmail.com>
2022-03-29 15:11:12 +0000

    create join table between treatments and medical_histories

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -17,8 +17,20 @@ CREATE TABLE medical_histories (
 );
 
 -- craete treatments table
-CREATE TABLE medical_histories (
+CREATE TABLE treatments (
   id SERIAL PRIMARY KEY,
   type VARCHAR(255),
-  name VARCHAR(50),
+  name VARCHAR(50)
 );
+
+-- create join table between treatments and medical_histories
+CREATE TABLE medical_histories_treatments (
+  medical_history_id INT NOT NULL,
+  treatment_id INT NOT NULL,
+  FOREIGN KEY (medical_history_id) 
+    REFERENCES medical_histories (id)
+    ON DELETE CASCADE,
+  FOREIGN KEY (treatment_id) 
+    REFERENCES treatments (id)
+    ON DELETE CASCADE
+)
